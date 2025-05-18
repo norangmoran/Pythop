d6673afb81de89e248beac77a240027ecf6b9db8
Remily <87406167+norangmoran@users.noreply.github.com>
2025-05-19 03:22:46 +0900

    관리자페이지 내 Post표기 수정
    등록일, 수정일 필드도 추가.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('custom_title', 'updated_at', 'created_at')
+
+    list_display_links = ('custom_title',)
+
+    def custom_title(self, obj):
+        return f"[{obj.pk}] {obj.title}"
+
+    custom_title.short_description = "목차"
+    Post._meta.get_field("updated_at").verbose_name = "수정일"
+    Post._meta.get_field("created_at").verbose_name = "등록일"
+
+admin.site.register(Post, PostAdmin)
