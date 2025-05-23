0c5047074f400f55e213ceb41c8442b02de3e76b
Remily <87406167+norangmoran@users.noreply.github.com>
2025-05-24 02:15:28 +0900

    FBV로 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html')
+    data_Post = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'DATA' : data_Post,
+        })
