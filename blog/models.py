5dbd8d82d0679fc1e95a674a72a3aa9edb4434c9
Remily <87406167+norangmoran@users.noreply.github.com>
2025-05-30 00:44:30 +0900

    FBV로 목록 페이지 만들기3
    models.py에서 이동할 페이지 url만들어주는 방식으로 수정

--- blog/models.py
+++ blog/models.py
@@ -8,4 +8,8 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'[ {self.pk} ] {self.title}'
+        return (f'[ {self.pk} '
+                f'] {self.title}')
+
+    def single_url_str(self):
+        return f'/blog/{self.pk}'
